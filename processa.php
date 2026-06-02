64207df0c042f0233bdaa147fe56a34e14e465c2
Eduarda <eduardaalcsantos@gmail.com>
2026-06-02 09:11:15 -0300

    Add files via upload

---
+++ processa.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-5 p-3 mb-2 bg-light text-dark">
+
+                <h1>Bem Vindo ao My Pocket!</h1>
+                <p>Por favor, selecione o tipo de transação.</p>
+
+                <button type="button" class="btn btn-success">Receita</button>
+                <button type="button" class="btn btn-danger">Despesa</button>
+                <button type="button" class="btn btn-secondary">Ver extrato</button>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
