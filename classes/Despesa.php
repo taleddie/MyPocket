a34ab62e763369defd2c43392c78f897cacc0e32
Eduarda <eduardaalcsantos@gmail.com>
2026-06-15 09:47:59 -0300

    Update Despesa.php

--- classes/Despesa.php
+++ classes/Despesa.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 require_once 'Transacao.php';
 class Despesa extends Transacao {
-    public function __construct($id, $data, $valor, $descricao) {
-        parent::__construct($id, $data, $valor, $descricao);
-    }
-
     public function getTipo() {
         return "Saída";
     }
